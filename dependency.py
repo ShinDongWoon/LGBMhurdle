6a0620140d825136488a95ad78d3e09fa3240b09
Castorp <50649074+ShinDongWoon@users.noreply.github.com>
2025-09-10 10:49:18 +0900

    Add holidayskr dependency

--- dependency.py
+++ dependency.py
@@ -8,6 +8,7 @@ REQUIRED_PACKAGES = [
     "lightgbm",
     "pyyaml",  # for reading YAML configuration files
     "scikit-learn",  # optional utilities for model evaluation
+    "holidayskr",
 ]
 
 def install_missing_packages(packages):
